e75c876d667c1bfbd1c3b7a5291dabdd51878e6e
KingWep <thaneverlose168@gmail.com>
2026-02-02 10:29:55 +0700

    Update Css with Render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+
 // use Symfony\Component\Routing\Route;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
             Route::redirect('/home', '/');
         }
         if (config('app.env') === 'production') {
-        \URL::forceScheme('https');
+        URL::forceScheme('https');
         }
     }
 }
